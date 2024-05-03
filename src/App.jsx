7d9ee3576872b8b16667594607742fe5b83baa67
mindula-madhuhansa <mimmindula@gmail.com>
2024-05-03 15:42:52 +0530

    chore: Add react-router-dom dependency and implement routing in App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 };
